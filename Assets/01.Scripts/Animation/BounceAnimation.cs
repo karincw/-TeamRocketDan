1562841ecf008972d56bd3b0e932ba561b7566d4
leo82380 <tkdrb062@gmail.com>
2024-12-18 08:53:41 +0900

    feat: Custom Animation

--- Assets/01.Scripts/Animation/BounceAnimation.cs
+++ Assets/01.Scripts/Animation/BounceAnimation.cs
@@ -5,17 +5,19 @@ namespace Mochi.Animation
 {
     public class BounceAnimation : MonoBehaviour
     {
-        [SerializeField] private float _duration = 0.5f;
+        [SerializeField] private float _speed = 0.5f;
+        [SerializeField] private float _scale = 1.2f;
+        [SerializeField] private Ease _ease = Ease.InQuad;
         
         private void Start()
         {
             Sequence sequence = DOTween.Sequence();
-            sequence.Append(transform.DOScaleX(1.2f, _duration / 2));
-            sequence.Append(transform.DOScaleX(1f, _duration / 2));
-            sequence.Join(transform.DOScaleY(1.2f, _duration / 2));
-            sequence.Append(transform.DOScaleY(1f, _duration / 2));
+            sequence.Append(transform.DOScaleX(_scale, _speed / 2));
+            sequence.Append(transform.DOScaleX(1f, _speed / 2));
+            sequence.Join(transform.DOScaleY(_scale, _speed / 2));
+            sequence.Append(transform.DOScaleY(1f, _speed / 2));
             sequence.SetLoops(-1);
-            sequence.SetEase(Ease.InQuad);
+            sequence.SetEase(_ease);
             sequence.Play();
         }
     }
