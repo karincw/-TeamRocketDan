e05d8e30936a927bbcde7cce23c5174da7151089
Karin <karin.cw07@gmail.com>
2024-12-17 18:43:17 +0900

    start

---
+++ Assets/01.Scripts/Share/InputReaderSO.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static Controls;
+
+[CreateAssetMenu(menuName = "SO/InputReader")]
+public class InputReaderSO : ScriptableObject, IPlayerActions, IUIActions
+{
+    private Controls _controls;
+    public Controls Controls => _controls;
+
+    private void OnEnable()
+    {
+        if (_controls == null)
+        {
+            _controls = new Controls();
+            _controls.Player.SetCallbacks(this);
+            _controls.UI.SetCallbacks(this);
+        }
+
+        _controls.Player.Enable();
+        _controls.UI.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Player.Disable();
+        _controls.UI.Disable();
+    }
+
+    public void OnNavigate(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnSubmit(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnCancel(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnRightClick(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnMiddleClick(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnScrollWheel(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnTrackedDevicePosition(InputAction.CallbackContext context)
+    {
+
+    }
+
+    public void OnTrackedDeviceOrientation(InputAction.CallbackContext context)
+    {
+
+    }
+}
