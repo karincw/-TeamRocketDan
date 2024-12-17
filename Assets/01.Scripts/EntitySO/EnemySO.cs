cb86aae4797fc412fb7c86e9893aa9e6705fa6fc
leo82380 <tkdrb062@gmail.com>
2024-12-17 23:47:17 +0900

    feat: Asset

---
+++ Assets/01.Scripts/EntitySO/EnemySO.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Mocchi.Entity.SO
+{
+    public class EnemySO : ScriptableObject
+    {
+        public Sprite sprite;
+        public int maxHealth;
+        public int damage;
+        public int defense;
+        public float speed;
+    }
+}
+
---
+++ Assets/01.Scripts/EntitySO/WaveSO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mocchi.Entity.SO
+{
+    [CreateAssetMenu(fileName = "Wave", menuName = "EntitySO/Wave")]
+    public class WaveSO : ScriptableObject
+    {
+        public List<EnemySO> enemies;
+    }
+}
