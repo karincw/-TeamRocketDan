add3d590599c4dcc5eaa1306bb7a1a9a535eb84c
yogurt-31 <syjang070813@gmail.com>
2024-12-18 11:14:18 +0900

    [20241218] 적생성 및 이동

---
+++ Assets/01.Scripts/JSY/Enemy/EnemyCreateManager.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EnemyCreateManager : MonoBehaviour
+{
+    [SerializeField] private Transform enemyParent;
+    [SerializeField] private Enemy enemyPrefab;
+    [SerializeField] private List<MovePoint> movePoints = new List<MovePoint>();
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            CreateEnemy();
+        }
+    }
+
+    private void CreateEnemy()
+    {
+        Enemy obj = Instantiate(enemyPrefab, movePoints[0].transform.position, Quaternion.identity, enemyParent);
+        obj.SetMovePoints(movePoints);
+    }
+}
