fa08785bb5da3e5da0f9dd7df3d891c048452e3e
leo82380 <tkdrb062@gmail.com>
2024-12-19 00:38:52 +0900

    feat: Meteor

---
+++ Assets/01.Scripts/Damage/Meteor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Leo.Interface;
+using UnityEngine;
+
+namespace Leo.Damage
+{
+    public class Meteor : MonoBehaviour
+    {
+        [SerializeField] private Transform _visual;
+        [SerializeField] private float _rotSpeed = 10f;
+        [SerializeField] private float _fallSpeed = 5f;
+        [SerializeField] private Vector2 _fallDir;
+
+        private void Update()
+        {
+            transform.Translate(_fallDir * (_fallSpeed * Time.deltaTime));
+            _visual.Rotate(Vector3.forward, _rotSpeed * Time.deltaTime);
+        }
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.TryGetComponent(out IStunable stun))
+            {
+                stun.Stun(5);
+            }
+        }
+    }
+}
