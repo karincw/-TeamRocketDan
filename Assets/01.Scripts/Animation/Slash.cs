f3f9400592c19226cf15b0d74588ab3723efca53
leo82380 <tkdrb062@gmail.com>
2024-12-18 10:45:40 +0900

    feat: Add Slash

---
+++ Assets/01.Scripts/Animation/Slash.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Mochi.Animation
+{
+    public class Slash : MonoBehaviour
+    {
+        [SerializeField] private GameObject _visual;
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartSlash();
+            }
+        }
+
+        [ContextMenu("StartSlash")]
+        public void StartSlash()
+        {
+            transform.DOKill();
+            transform.DORotate(new Vector3(0, 0, -90), 0f).OnComplete(() => _visual.SetActive(true));
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(transform.DORotate(new Vector3(0, 0, -90), 0.2f))
+                .Join(transform.DORotate(new Vector3(0, 0, 0), 0.2f))
+                .Join(transform.DORotate(new Vector3(0, 0, 90), 0.2f))
+                .SetEase(Ease.Linear)
+                .OnComplete(() => _visual.SetActive(false));
+            
+            sequence.Play();
+        }
+        
+        #if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, 0.5f);
+        }
+        #endif
+    }
+}
--- Assets/01.Scripts/Core/CameraManager.cs
+++ Assets/01.Scripts/Core/CameraManager.cs
@@ -1,22 +1,25 @@
 using Unity.Cinemachine;
 
-public class CameraManager : MonoSingleton<CameraManager>
+namespace Mochi.Core
 {
-    private CinemachineImpulseSource _impulseSource;
+    public class CameraManager : MonoSingleton<CameraManager>
+    {
+        private CinemachineImpulseSource _impulseSource;
 
-    protected override void Awake()
-    {
-        base.Awake();
-        _impulseSource = GetComponent<CinemachineImpulseSource>();
-    }
-    
-    public void ShakeCamera(float force)
-    {
-        GenerateImpulse(force);
-    }
-    
-    private void GenerateImpulse(float force)
-    {
-        _impulseSource.GenerateImpulse(force);
+        protected override void Awake()
+        {
+            base.Awake();
+            _impulseSource = GetComponent<CinemachineImpulseSource>();
+        }
+
+        public void ShakeCamera(float force)
+        {
+            GenerateImpulse(force);
+        }
+
+        private void GenerateImpulse(float force)
+        {
+            _impulseSource.GenerateImpulse(force);
+        }
     }
 }
