10fa8efb41fd72e0744e774241b58c7acf562721
leo82380 <tkdrb062@gmail.com>
2024-12-18 23:30:46 +0900

    feat: Particles

---
+++ Assets/01.Scripts/Damage/Particles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Leo.Interface;
+using UnityEngine;
+
+namespace Leo.Damage
+{
+    public class Particles : MonoBehaviour, IEffectable
+    {
+        [SerializeField] private ParticleSystem _particleSystem;
+        private DamageCaster _damageCaster;
+
+        private void Awake()
+        {
+            _damageCaster = GetComponent<DamageCaster>();
+        }
+
+        public void SetPos(Transform target)
+        {
+            transform.position = target.position;
+        }
+
+        public void Play()
+        {
+            _particleSystem.Play();
+        }
+
+        public DamageCaster GetDamageCaster()
+        {
+            return _damageCaster;
+        }
+    }
+}
