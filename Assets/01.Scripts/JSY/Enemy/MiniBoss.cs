5fd6bf8bb09bd7ad0797254da18c5a05becca38a
leo82380 <tkdrb062@gmail.com>
2024-12-19 16:00:09 +0900

    feat: Mini Boss

---
+++ Assets/01.Scripts/JSY/Enemy/MiniBoss.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace JSY
+{
+    public class MiniBoss : Enemy
+    {
+        [SerializeField] private float _liftTime;
+        [SerializeField] private TextMeshPro _lifeText;
+        [SerializeField] private ParticleSystem _particleSystem;
+        
+        private float _timer;
+        protected override void Start()
+        {
+            base.Start();
+            transform.localScale = new Vector3(1.2f, 1.2f, 1f);
+            _timer = _liftTime;
+            Destroy(gameObject, _liftTime);
+        }
+        
+        public void SetLifeText(int life)
+        {
+            _lifeText.text = life.ToString();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            _timer -= Time.deltaTime;
+            SetLifeText(Mathf.RoundToInt(_timer));
+        }
+
+        private void OnDestroy()
+        {
+            var particles = Instantiate(_particleSystem, transform.position, Quaternion.identity);
+            particles.Play();
+        }
+    }
+}
