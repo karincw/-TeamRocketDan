db65c5aba3f733a8bf110748b8472ec52f6add3b
LEE SANG GYU <tkdrb062@gmail.com>
2025-04-07 12:59:21 +0900

    fix: Boss Warning

--- Assets/01.Scripts/JSY/Enemy/EnemyCreateManager.cs
+++ Assets/01.Scripts/JSY/Enemy/EnemyCreateManager.cs
@@ -64,7 +64,7 @@ namespace JSY
             {
                 VolumeManager.Instance.GetComponent<Vignette>().color.DOColor(Color.red, 0.5f,
                     () => VolumeManager.Instance.GetComponent<Vignette>().color.DOColor(Color.black, 0.5f));
-                yield return new WaitForSeconds(0.5f);
+                yield return new WaitForSeconds(1f);
             }
         }
 
