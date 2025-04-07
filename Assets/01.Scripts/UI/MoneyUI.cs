5f747b474749a2137db095b5e4b9d9e8d0097461
yogurt-31 <123716140+yogurt-31@users.noreply.github.com>
2025-04-07 13:00:20 +0900

    [20250407] 씬수정

--- Assets/01.Scripts/UI/MoneyUI.cs
+++ Assets/01.Scripts/UI/MoneyUI.cs
@@ -13,8 +13,8 @@ namespace JSY
         [SerializeField] private MochiDataSO data;
         [SerializeField] private TextMeshProUGUI moneyText;
         [SerializeField] private TextMeshProUGUI costText;
+        [SerializeField] private int money = 162;
         private Button buyButton;
-        private int money = 0;
         private int cost = 40;
 
         [SerializeField] private bool _debugMode = false;
@@ -23,7 +23,6 @@ namespace JSY
         {
             buyButton = bottomUITrm.Find("BuyBtn").GetComponent<Button>();
             buyButton.onClick.AddListener(HandleBuyButton);
-            ModifyMoney(162);
         }
 
         private void Update()
